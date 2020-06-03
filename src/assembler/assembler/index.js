9160c9a22b5ddf9f26c3ac52326b73ea838e8f2e
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-06-03 16:05:12 +0200

    Create index.js

---
+++ src/assembler/assembler/index.js
@@ -0,0 +1,55 @@
+// encoding the core constants and prefixes
+
+const instructionEncoder = require('./instruction-encoder');
+
+const {
+  getInstructionType,
+  getInstructionArguments,
+  validateInstruction
+} = require('./assembler-utils');
+
+const {
+  DIRECT_ASSIGNMENT,
+  STRING_ASSIGNMENT
+} = require('../../constants');
+
+module.exports = (preprocessedObject) => {
+  const { instructions, data } = preprocessedObject;
+  const dataLabels = Object.keys(data);
+
+  const dataSize = dataLabels
+    .reduce((acc, cur) => acc + data[cur].size, 0);
+
+  const out = new Uint16Array(instructions.length + dataSize);
+  instructions
+    .map(instruction => instruction.toUpperCase())
+    .forEach((instruction, index) => {
+      validateInstruction(instruction);
+      const iType = getInstructionType(instruction);
+      const iArgs = getInstructionArguments(instruction);
+      const encoded = instructionEncoder[iType](iArgs);
+
+      out[index] = encoded;
+    });
+    
+  console.log(data)
+  
+  // Place the data into the assembled buffer
+  dataLabels
+    .forEach((label) => {
+      const dataDescriptor = data[label];
+      const index = dataDescriptor.address;
+      if (dataDescriptor.type === DIRECT_ASSIGNMENT) {
+        out[index] = dataDescriptor.data;
+      } else if (dataDescriptor.type === STRING_ASSIGNMENT) {
+        dataDescriptor.data
+          .split('')
+          .forEach((chr, i) => {
+            out[index + i] = chr.charCodeAt(0);
+          });
+        out[index + dataDescriptor.size] = 0;
+      }
+    });
+
+  return out;
+}
