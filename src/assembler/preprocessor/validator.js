8dee22db374c15f3f3659281067db4703a32d028
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-06-03 16:31:48 +0200

    Create validator.js

---
+++ src/assembler/preprocessor/validator.js
@@ -0,0 +1,19 @@
+// throw errors
+
+module.exports = (lines) => {
+  const checkForDirective = (directive) => {
+    if (lines.indexOf(directive) < 0) {
+      throw new Error(`No ${directive} directive found. Exiting...`);
+    }
+  };
+  ['.data', '.text'].forEach(checkForDirective);
+
+  const foundGlobal = lines.some(line => {
+    const directive = line.split(' ')[0];
+    return directive === '.global';
+  });
+
+  if (!foundGlobal) {
+    throw new Error('No .global directive found. Exiting...');
+  }
+}
