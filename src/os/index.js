dd52e990776369ee25034acca9f5a5fde8bd2a41
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-06-03 16:17:03 +0200

    Create index.js

---
+++ src/os/index.js
@@ -0,0 +1,14 @@
+const { OS } = require('../constants');
+const stdout = require('./stdout');
+const stdin = require('./stdin');
+
+module.exports = (registers, memory) => {
+  switch (registers.A) {
+    case OS.STDOUT:
+      stdout(registers.B, registers.C, memory);
+      break;
+    case OS.STDIN:
+      registers.B = stdin();
+      break;
+  }
+};
