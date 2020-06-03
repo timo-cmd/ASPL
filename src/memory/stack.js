f15623503029e79bd9e8d6d5555400dcfa643f9d
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-06-03 16:12:06 +0200

    Create stack.js

---
+++ src/memory/stack.js
@@ -0,0 +1,23 @@
+// defining the current stack
+// catching bugs while setting the stacks 
+
+const { STACK_SIZE } = require('../constants');
+const stack = new Uint16Array(STACK_SIZE);
+
+module.exports = (registers) =>
+  ({
+    push: (val) => {
+      if (registers.SP === STACK_SIZE - 1) {
+        throw new Error('[Error] Stack overflow. Exiting...');
+      }
+      stack[registers.SP++] = val;
+    },
+
+    pop: () => {
+      if (registers.SP === 0) {
+        throw new Error('[Error] Stack underflow. Exiting...');
+      }
+      return stack[--registers.SP];
+    },
+    raw: stack
+  })
