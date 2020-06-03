ff7f9d001816d8eebb7da164fdd3d09b21e83122
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-06-03 15:50:25 +0200

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,57 @@
+// main util entrypoint for debug and asm vm
+
+const colors = require('colors/safe');
+const { readFile, writeFile, stat } = require('fs');
+const fs = require('bluebird').promisifyAll({ readFile, writeFile, stat });
+
+const {
+  DESTINATION_SHIFT,
+  SOURCE_SHIFT,
+  ADDRESS_SHIFT,
+  LONG_ADDRESS_SHIFT
+} = require('./constants');
+
+const leftPad = (str, pad = 4, padWith = '0') =>
+  (str.length < pad)
+    ? Array.apply(null, { length: pad - str.length })
+      .reduce((padding) => padding + padWith, '') + str
+    : str;
+
+const arrayAsHex = (arr, startingOffset = 0, ip = -1) => {
+  let s = `${leftPad(startingOffset.toString(16))}\t`;
+  for (let i = 0; i < arr.length; i++) {
+    const fourBitsPadded = leftPad(arr[i].toString(16), 4);
+
+    let cf = x => x;
+    if (i === ip) cf = colors.yellow;
+    s += cf(fourBitsPadded) + ' ';
+
+    if (((i + 1) % 16 === 0)) s += `\n${leftPad((startingOffset + i + 1).toString(16))}\t`;
+  }
+  return s;
+};
+const convertUint8ArrayToUint16Array = (u8) => {
+  const u16 = new Uint16Array(u8.length / 2);
+  u8.forEach((_, i) => {
+    if (i % 2 !== 0) {
+      u16[(i - 1) / 2] = u8[i - 1] | (u8[i] << 8);
+    }
+  });
+  return u16;
+};
+const splitInstruction = (instruction) => [
+  (instruction & 0b0000000000001111),
+  (instruction & 0b0000000000110000) >> DESTINATION_SHIFT,
+  (instruction & 0b0000000011000000) >> SOURCE_SHIFT,
+  (instruction & 0b1111111100000000) >> ADDRESS_SHIFT,
+  (instruction & 0b1111111111000000) >> LONG_ADDRESS_SHIFT
+];
+
+module.exports = {
+  leftPad,
+  arrayAsHex,
+  convertUint8ArrayToUint16Array,
+  splitInstruction,
+
+  fs
+};
