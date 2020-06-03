085b18aafc11ed2f6461e434688ecc4090c952fd
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-06-03 16:32:05 +0200

    Create utils.js

---
+++ src/assembler/preprocessor/utils.js
@@ -0,0 +1,38 @@
+const unescapeCharacters = (str) =>
+  str
+    .replace(/\\n/g, '\n')
+    .replace(/\\r/g, '\r')
+    .replace(/\\f/g, '\f')
+    .replace(/\\b/g, '\b')
+    .replace(/\\t/g, '\t')
+    .replace(/\\v/g, '\v')
+    .replace(/\\;/, ';');
+
+const getInstructionArguments = (instruction) =>
+  instruction
+    .split(/^[a-zA-Z0-9]+?\s/)[1]
+    .split(',')
+    .map(x => x.trim());
+
+const uniqueLabel = () =>
+  Math
+    .random()
+    .toString(16)
+    .slice(2) + ':';
+
+const getUsableRegister = (...unusableRegisters) => {
+  const usableRegisters = ['B', 'C', 'D'];
+  return usableRegisters
+    .filter(r =>
+      unusableRegisters
+        .map(reg => reg.toUpperCase())
+        .indexOf(r) < 0
+    )[0];
+}
+
+module.exports = {
+  getInstructionArguments,
+  uniqueLabel,
+  unescapeCharacters,
+  getUsableRegister
+};
