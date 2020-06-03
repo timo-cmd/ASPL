c20c2badc2a4f828912e70f6e6328b3c4e4418bd
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-06-03 16:17:34 +0200

    Create stdin.js

---
+++ src/os/stdin.js
@@ -0,0 +1,19 @@
+// catching SIGINT instructions
+
+const readline = require('readline');
+const stdinBuffer = new Uint16Array(1);
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+process.stdin.on('keypress', (str, key) => {
+  if (key.ctrl && key.name === 'c') {
+    process.exit();
+  }
+  stdinBuffer[0] = key.sequence.charCodeAt(0);
+});
+
+module.exports = () => {
+  const value = stdinBuffer[0];
+  stdinBuffer[0] = 0;
+  return value;
+}
