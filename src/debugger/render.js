b2c84d35a5d71e2a311d8ea607095bf7214ac6e2
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-06-03 16:21:34 +0200

    Create render.js

---
+++ src/debugger/render.js
@@ -0,0 +1,32 @@
+// cli for rendering single debugged mem.pages
+
+const {
+  leftPad,
+  arrayAsHex,
+  splitInstruction
+} = require('../utils');
+const { INSTRUCTION_MAP, DEBUG } = require('../constants');
+
+const registers = require('../cpu/registers')
+const _memory = require('../memory');
+const memory = _memory.memory;
+const stack = _memory.stack(registers).raw;
+
+const pagedMemory = require('./memory-pages');
+
+module.exports = (memoryPage) => {
+  const instruction = memory[registers.IP];
+  const opcode = splitInstruction(instruction)[0];
+  const namedOpcode = INSTRUCTION_MAP[opcode];
+
+  const regs = Object.keys(registers)
+    .reduce((outputStr, registerName) =>
+      `${outputStr}${registerName}: ${leftPad(registers[registerName].toString(16))}\t`,
+    '');
+  console.log(`Memory:\n${pagedMemory(memory, memoryPage, registers.IP)}\nPage ${memoryPage + 1}/${DEBUG.NUM_PAGES}\n`);
+  console.log(`Stack:\n${arrayAsHex(stack)}\n`);
+  console.log(`Instruction: (${namedOpcode}) ${leftPad(instruction.toString(2), 16)}`);
+  console.log(`Registers:\n${regs}\n`);
+
+  process.stdout.write('Press: [s]: step | [e]: exit | [n]: next | [p]: previous memory page>  ');
+};
