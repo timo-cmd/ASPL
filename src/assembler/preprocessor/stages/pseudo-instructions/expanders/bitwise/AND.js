cb54c1aa8570be916c537bc4de9ee01d807ceded
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-06-05 07:29:15 +0200

    Create AND.js

---
+++ src/assembler/preprocessor/stages/pseudo-instructions/expanders/bitwise/AND.js
@@ -0,0 +1,8 @@
+// arithmethic and operator
+
+const { getInstructionArguments } = require('../../../../utils');
+
+module.exports = (instruction) => {
+  const [destination, source] = getInstructionArguments(instruction);
+  return [`ATH ${destination}, ${source}, 8, 0, 0`];
+}
